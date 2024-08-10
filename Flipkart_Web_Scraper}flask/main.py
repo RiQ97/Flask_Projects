94bf8ea8e94a875d33e4b97332ae0d13d57bdca1
RiQ97 <unknownritikraj@gmail.com>
2024-08-10 21:10:06 +0530

    project 3rd

--- Flipkart_Web_Scraper}flask/main.py
+++ Flipkart_Web_Scraper}flask/main.py
@@ -1,12 +1,11 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from flask_cors import CORS, cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 import pymongo
-import certifi
 import ssl
 
 load_dotenv()
